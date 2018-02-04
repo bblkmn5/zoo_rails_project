bdbf86a8d3fea14f719bb582820a722423b4358b
Ben Blackman <bblkmn5@gmail.com>
2018-02-04 13:12:57 -0800

    start adding user login parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UsersController < ApplicationController
+    before_action :set_user, only: [:show, :edit, :update]
+
+    def index
+        @users = User.all
+    end
+
+    def show
+        @message = params[:message] if params[:message]
+
+        if session[:user_id] != @user.id
+            redirect_to root_path
+        end
+    end
+
+    def new
+        @user = User.new
+    end
+
+    def create
+        @user = User.new(user_params)
+        if @user.save
+            session[:user_id] = @user.id
+            redirect_to user_path(@user)
+        else
+            redirect_to root_path
+        end
+    end
+    
+    def edit
+    end
+
+    def update
+        if @user.update(user_params)
+            redirect_to @user
+        else
+            render :edit
+        end 
+    end
+
+    private
+
+    def set_user
+        @user = User.find(params[:id])
+    end
+
+    def user_params
+        params.require(:user).permit(:username, :password, :password_confirmation, :email, :zoo_id)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ApplicationRecord
     has_one :zoo
+    has_many :animal_keepers
+    has_many :animals, through: :animal_keepers
+    has_many :keepers, through: :animal_keepers
 
     has_secure_password
 end
