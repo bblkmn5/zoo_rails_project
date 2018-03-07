a846a0d79111f7dab6af7974907bc0c3d1eadf87
BenB <bblkmn5@gmail.com>
2018-03-07 10:32:10 -0800

    update route urls, delete unnecessary code.

--- config/routes.rb
+++ config/routes.rb
@@ -4,20 +4,13 @@ Rails.application.routes.draw do
   
   root 'static#home', as: 'root'
 
-  get 'zoos/other_zoo' => 'zoos#other_zoo', as: 'other_zoos'
+  get 'other_zoos' => 'zoos#other_zoo', as: 'other_zoos'
+  get 'users/all_animals' => 'animals#index', as: 'all_animals'
+  get 'users/all_keepers' => 'keepers#index', as: 'all_keepers'
 
   resources :users, only: [:show] do
     resources :zoos
     resources :animals
     resources :keepers
   end
-
-  # resources :zoos do
-  #   resources :animals
-  #   resources :keepers
-  # end
-  
-  # resources :zoos
-  # resources :animals
-  # resources :keepers
 end
