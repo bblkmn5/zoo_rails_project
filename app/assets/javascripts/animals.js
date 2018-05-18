a3334ccb1993410e5940f866dcfa24dbd013a882
BenB <bblkmn5@gmail.com>
2018-05-17 19:24:47 -0700

    update spec-js, remove unused code, format animal show page

--- app/assets/javascripts/animals.js
+++ app/assets/javascripts/animals.js
@@ -9,34 +9,11 @@ $(function() {
       let animalComments = $.map(animal.comments, function(comment) {
           return `<li>${comment.notes}</li>`
         })
-      console.log(animalComments) 
+      // console.log(animalComments) 
       let animalTraits = `<p>Species: ${animal.species}</p><p>Personality: ${animal.personality}</p><p>Notes: <ol>${animalComments.toString().replace(/,/g , '')}</ol>`;
       $("#animal-" + id).html(animalTraits);
       })
     });
-
-  $(".js-next").on("click", function (e) {
-    e.preventDefault();
-    let id = $(this).data("id");
-    let zoo_id = $(this).data("zoo-id");
-    let user_id = $(this).data("user-id");
-    let nextID = parseInt($(".js-next").attr("data-id"));
-    
-    $.get(`/users/${user_id}/zoos/${zoo_id}.json`, function (data) {
-      let animals = data.animals;
-      
-      for (animal of animals){
-        $(".animalTraits").html(`${animal.name} the ${animal.species}`);
-      $(".animalPersonality").html(`Personality: ${animal.personality}<br>`);
-      }
-      let animalComments = $.map(animal.comments, function(comment) {
-        return `<li>${comment.notes}</li>`
-      })
-      $(".comments").html(`<ol>${animalComments.toString().replace(/,/g , '')}</ol>`)
-      // re-set the id to current on link
-      $(".js-next").attr("data-id", animal.id);
-    });
-  });
 
   $("#new_comment").on("submit", function(e) {
     e.preventDefault();
